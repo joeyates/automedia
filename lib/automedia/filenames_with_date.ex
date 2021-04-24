5989ea21fe44de568dcc6d1747b10d5fde026255
Joe Yates <git@joeyates.info>
2021-04-24 21:46:20 +0000

    Handle filenames with milliseconds

--- lib/automedia/filenames_with_date.ex
+++ lib/automedia/filenames_with_date.ex
@@ -2,6 +2,7 @@ defmodule Automedia.FilenamesWithDate do
   @moduledoc false
 
   @with_date_and_second ~r[\/(?:IMG|VID)_(\d{4})(\d{2})(\d{2})_(\d{2})(\d{2})(\d{2})\.(jpe?g|mp4)]
+  @with_date_and_millisecond ~r[\/(?:PXL)_(\d{4})(\d{2})(\d{2})_(\d{2})(\d{2})(\d{2})(\d{3})\.(jpe?g|mp4)]
 
   def find(path) do
     list_files(path)
@@ -15,7 +16,8 @@ defmodule Automedia.FilenamesWithDate do
   end
 
   defp match(pathname) do
-    match_with_date_and_second(pathname)
+    match_with_date_and_second(pathname) ||
+      match_with_date_and_millisecond(pathname)
   end
 
   @spec match_with_date_and_second(Automedia.Movable.t()) :: Automedia.Movable.t() | nil
@@ -28,6 +30,27 @@ defmodule Automedia.FilenamesWithDate do
     if match do
       [year, month, day, hour, minute, second, extension] = match
       {:ok, time} = Time.new(i(hour), i(minute), i(second))
+      %Automedia.Movable{
+        source: pathname,
+        year: String.to_integer(year),
+        month: String.to_integer(month),
+        day: String.to_integer(day),
+        time: time,
+        extension: extension
+      }
+    end
+  end
+
+  @spec match_with_date_and_millisecond(Automedia.Movable.t()) :: Automedia.Movable.t() | nil
+  defp match_with_date_and_millisecond(pathname) do
+    match = Regex.run(
+      @with_date_and_millisecond,
+      pathname,
+      capture: :all_but_first
+    )
+    if match do
+      [year, month, day, hour, minute, second, millisecond, extension] = match
+      {:ok, time} = Time.new(i(hour), i(minute), i(second), i(millisecond) * 1000)
       %Automedia.Movable{
         source: pathname,
         year: String.to_integer(year),
