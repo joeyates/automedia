427246be75bd1791bc7b14b1225fbbb51861a99c
Joe Yates <joe.g.yates@gmail.com>
2021-04-25 17:15:27 +0200

    Use the logger

--- lib/automedia/move.ex
+++ lib/automedia/move.ex
@@ -1,18 +1,20 @@
 defmodule Automedia.Move do
   @moduledoc false
 
+  require Logger
+
   require Automedia.Movable
 
   def move(%Automedia.Movable{source: source, destination: destination}) do
     path = Path.dirname(destination)
     if !File.dir?(path) do
-      IO.puts "Creating directory '#{path}'"
+      Logger.info "Creating directory '#{path}'"
       File.mkdir_p!(path)
     end
     if File.regular?(destination) do
-      IO.puts "Skipping move of '#{source}' to '#{destination}' - destination file already exists"
+      Logger.info "Skipping move of '#{source}' to '#{destination}' - destination file already exists"
     else
-      IO.puts "Moving '#{source}' to '#{destination}'"
+      Logger.info "Moving '#{source}' to '#{destination}'"
       File.rename!(source, destination)
     end
   end
