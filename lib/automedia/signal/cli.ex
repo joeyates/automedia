75e5fad2fd2a912ebf527495686e6f7cd02cefaf
Joe Yates <git@joeyates.info>
2021-04-25 16:52:54 +0000

    Optionally update Signal start timestamp file

--- lib/automedia/signal/cli.ex
+++ lib/automedia/signal/cli.ex
@@ -31,6 +31,8 @@ defmodule Automedia.Signal.CLI do
     if length(movable) == 0, do: Logger.debug "No Signal files found"
 
     Enum.map(movable, &(Automedia.Move.move(&1, dry_run: options.dry_run)))
+
+    optionally_update_start_timestamp_file(options, movable)
   end
 
   defp start_datetime(%{start_timestamp_file: pathname}) do
@@ -46,4 +48,21 @@ defmodule Automedia.Signal.CLI do
     end
   end
   defp start_datetime(_options), do: nil
+
+  def optionally_update_start_timestamp_file(_options, []), do: nil
+  def optionally_update_start_timestamp_file(%{dry_run: true}, _movable), do: nil
+  def optionally_update_start_timestamp_file(%{start_timestamp_file: pathname}, movable) do
+    latest = Enum.max(movable, fn m1, m2 ->
+      cond do
+        m1.date < m2.date -> 2
+        m1.date > m2.date -> 1
+        m1.time < m2.time -> 2
+        m1.time > m2.time -> 1
+        true              -> 0
+      end
+    end)
+    timestamp = DateTime.new!(latest.date, latest.time, "Etc/UTC") |> DateTime.to_unix()
+    File.write!(pathname, Integer.to_string(timestamp))
+  end
+  def optionally_update_start_timestamp_file(_options, _movable), do: nil
 end
