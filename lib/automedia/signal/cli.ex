380c112ca332967d74ef74b8398a61bc4d12d4ee
Joe Yates <git@joeyates.info>
2021-05-05 17:32:51 +0000

    Extract module

--- lib/automedia/signal/cli.ex
+++ lib/automedia/signal/cli.ex
@@ -3,9 +3,7 @@ defmodule Automedia.Signal.CLI do
 
   require Logger
 
-  import Automedia.ConversionHelpers, only: [i_or_nil: 1]
-
-  @switches [
+  @move_switches [
     destination: :string,
     dry_run: :boolean,
     start_timestamp_file: :string,
@@ -14,61 +12,19 @@ defmodule Automedia.Signal.CLI do
     verbose: :count
   ]
 
-  @required [:source, :destination]
+  @move_required ~w(destination source)a
 
   def run(args) do
     case Automedia.OptionParser.run(
           args,
-          switches: @switches,
-          required: @required
+          switches: @move_switches,
+          required: @move_required
         ) do
       {:ok, options, []} ->
-        move_signal_files(options)
+        Automedia.Signal.Move.run(options)
       {:error, message} ->
         Logger.error message
         exit(1)
     end
   end
-
-  defp move_signal_files(options) do
-    if options.dry_run, do: Logger.debug "This is a dry run, nothing will be changed"
-
-    start = start_datetime(options)
-
-    movable =
-      options.source
-      |> Automedia.Signal.Movable.find(from: start)
-      |> Automedia.DestinationChooser.run(options.destination)
-
-    if length(movable) == 0, do: Logger.debug "No Signal files found"
-
-    Enum.map(movable, &(Automedia.Move.move(&1, dry_run: options.dry_run)))
-
-    optionally_update_start_timestamp_file(options, movable)
-  end
-
-  defp start_datetime(%{start_timestamp_file: pathname}) do
-    if File.regular?(pathname) do
-      timestamp = File.read!(pathname) |> i_or_nil
-      if timestamp do
-        dt = DateTime.from_unix!(timestamp)
-        Logger.debug "The Signal start timestamp file indicates only Signal files created after #{dt} are to be considered"
-      end
-      timestamp
-    else
-      Logger.debug "The Signal start timestamp file does not yet exist - all matching files will be collected"
-    end
-  end
-  defp start_datetime(_options), do: nil
-
-  def optionally_update_start_timestamp_file(_options, []), do: nil
-  def optionally_update_start_timestamp_file(%{dry_run: true}, _movable), do: nil
-  def optionally_update_start_timestamp_file(%{start_timestamp_file: pathname}, movable) do
-    timestamp =
-      movable
-      |> Enum.map(&(DateTime.new!(&1.date, &1.time) |> DateTime.to_unix()))
-      |> Enum.max()
-    File.write!(pathname, Integer.to_string(timestamp))
-  end
-  def optionally_update_start_timestamp_file(_options, _movable), do: nil
 end
---
+++ lib/automedia/signal/move.ex
@@ -0,0 +1,45 @@
+defmodule Automedia.Signal.Move do
+  import Automedia.ConversionHelpers, only: [i_or_nil: 1]
+
+  def run(options) do
+    if options.dry_run, do: Logger.debug "This is a dry run, nothing will be changed"
+
+    start = start_datetime(options)
+
+    movable =
+      options.source
+      |> Automedia.Signal.Movable.find(from: start)
+      |> Automedia.DestinationChooser.run(options.destination)
+
+    if length(movable) == 0, do: Logger.debug "No Signal files found"
+
+    Enum.map(movable, &(Automedia.Move.move(&1, dry_run: options.dry_run)))
+
+    optionally_update_start_timestamp_file(options, movable)
+  end
+
+  defp start_datetime(%{start_timestamp_file: pathname}) do
+    if File.regular?(pathname) do
+      timestamp = File.read!(pathname) |> i_or_nil
+      if timestamp do
+        dt = DateTime.from_unix!(timestamp)
+        Logger.debug "The Signal start timestamp file indicates only Signal files created after #{dt} are to be considered"
+      end
+      timestamp
+    else
+      Logger.debug "The Signal start timestamp file does not yet exist - all matching files will be collected"
+    end
+  end
+  defp start_datetime(_options), do: nil
+
+  def optionally_update_start_timestamp_file(_options, []), do: nil
+  def optionally_update_start_timestamp_file(%{dry_run: true}, _movable), do: nil
+  def optionally_update_start_timestamp_file(%{start_timestamp_file: pathname}, movable) do
+    timestamp =
+      movable
+      |> Enum.map(&(DateTime.new!(&1.date, &1.time) |> DateTime.to_unix()))
+      |> Enum.max()
+    File.write!(pathname, Integer.to_string(timestamp))
+  end
+  def optionally_update_start_timestamp_file(_options, _movable), do: nil
+end
