0f9e6de1d45333bee9fdec81fe98a4a5fb5f15fd
Joe Yates <git@joeyates.info>
2021-05-05 12:22:30 +0000

    Handle non-switch arguments

--- lib/automedia/option_parser.ex
+++ lib/automedia/option_parser.ex
@@ -6,10 +6,11 @@ defmodule Automedia.OptionParser do
 
   @doc false
   def run(args, options \\ []) do
-    with {:ok, switches} <- parse(args, options),
+    with {:ok, switches, remaining} <- parse(args, options),
          {:ok} <- check_required(switches, options),
+         {:ok} <- check_remaining(remaining, options),
          {:ok} <- setup_logger(switches) do
-      {:ok, switches}
+      {:ok, switches, remaining}
     else
       {:error, message} ->
         {:error, message}
@@ -20,21 +21,12 @@ defmodule Automedia.OptionParser do
     switches = Keyword.get(options, :switches, [])
 
     case OptionParser.parse(args, strict: switches) do
-      {switch_list, [], []} ->
+      {switch_list, remaining, []} ->
         switches = Enum.into(switch_list, %{})
-        {:ok, switches}
-      {_, remaining, []} ->
-        {:error, "Unexpected non-switch parameters supplied: #{inspect(remaining)}"}
-      {_, [], invalid} ->
+        {:ok, switches, remaining}
+      {_, _, invalid} ->
         keys = Enum.map(invalid, fn {key, _value} -> key end)
         {:error, "Unexpected parameters supplied: #{inspect(keys)}"}
-      {_, remaining, invalid} ->
-        keys = Enum.map(invalid, fn {key, _value} -> key end)
-        {
-          :error,
-          "Unexpected parameters supplied: #{inspect(keys)}, " <>
-            "and unexpected non-switch parameters supplied: #{inspect(remaining)}"
-        }
     end
   end
 
@@ -46,6 +38,15 @@ defmodule Automedia.OptionParser do
     else
       {:error, "Please supply the following parameters: #{inspect(missing)}"}
     end
+  end
+
+  defp check_remaining(_remaining, remaining: nil), do: {:ok}
+  defp check_remaining(remaining, remaining: count)
+  when length(remaining) == count do
+    {:ok}
+  end
+  defp check_remaining(_remaining, remaining: count) do
+    {:error, "Supply #{count} non-switch arguments"}
   end
 
   defp setup_logger(switches) do
