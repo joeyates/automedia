9dbbc8696e089f5679c025251c12511329025b70
Joe Yates <git@joeyates.info>
2021-05-09 22:00:31 +0000

    Bugfix: pattern match on the failing cases first

--- lib/automedia/destination_chooser.ex
+++ lib/automedia/destination_chooser.ex
@@ -9,30 +9,110 @@ defmodule Automedia.DestinationChooser do
 
     iex> Automedia.DestinationChooser.run([
     ...>   %Automedia.Movable{
-    ...>     date: Date.new!(2020, 1, 1),
+    ...>     date: ~D[2020-01-01],
     ...>     extension: "ext",
     ...>     source: nil,
-    ...>     time: Time.new!(15, 1, 1)
+    ...>     time: ~T[15:01:01]
     ...>   }
     ...> ], "/base")
-    [%Automedia.Movable{date: ~D[2020-01-01], destination: "/base/2020s/2020/202001/20200101/150101.ext", extension: "ext", source: nil, time: ~T[15:01:01]}]
+    [
+      %Automedia.Movable{
+        date: ~D[2020-01-01],
+        destination: "/base/2020s/2020/202001/20200101/150101.ext",
+        extension: "ext",
+        source: nil,
+        time: ~T[15:01:01]
+      }
+    ]
 
   When the supplied time includes a fractional part, it includes milliseconds:
+
     iex> Automedia.DestinationChooser.run([
     ...>   %Automedia.Movable{
-    ...>     date: Date.new!(2020, 1, 1),
+    ...>     date: ~D[2020-01-01],
     ...>     extension: "ext",
     ...>     source: nil,
-    ...>     time: Time.new!(15, 1, 1, {123000, 3})
+    ...>     time: ~T[15:01:01.123]
     ...>   }
     ...> ], "/base")
-    [%Automedia.Movable{date: ~D[2020-01-01], destination: "/base/2020s/2020/202001/20200101/150101123.ext", extension: "ext", source: nil, time: ~T[15:01:01.123]}]
+    [
+      %Automedia.Movable{
+        date: ~D[2020-01-01],
+        destination: "/base/2020s/2020/202001/20200101/150101123.ext",
+        extension: "ext",
+        source: nil,
+        time: ~T[15:01:01.123]
+      }
+    ]
+
+  When the date is missing, it does not set the destination:
+
+    iex> Automedia.DestinationChooser.run([
+    ...>   %Automedia.Movable{
+    ...>     date: nil,
+    ...>     extension: "ext",
+    ...>     source: nil,
+    ...>     time: ~T[15:01:01]
+    ...>   }
+    ...> ], "/base")
+    [
+      %Automedia.Movable{
+        date: nil,
+        destination: nil,
+        extension: "ext",
+        source: nil,
+        time: ~T[15:01:01]
+      }
+    ]
+
+  When the time is missing, it does not set the destination:
+
+    iex> Automedia.DestinationChooser.run([
+    ...>   %Automedia.Movable{
+    ...>     date: ~D[2020-01-01],
+    ...>     extension: "ext",
+    ...>     source: nil,
+    ...>     time: nil
+    ...>   }
+    ...> ], "/base")
+    [
+      %Automedia.Movable{
+        date: ~D[2020-01-01],
+        destination: nil,
+        extension: "ext",
+        source: nil,
+        time: nil
+      }
+    ]
+
+  When the extension is missing, it does not set the destination:
+
+    iex> Automedia.DestinationChooser.run([
+    ...>   %Automedia.Movable{
+    ...>     date: ~D[2020-01-01],
+    ...>     extension: nil,
+    ...>     source: nil,
+    ...>     time: ~T[15:01:01]
+    ...>   }
+    ...> ], "/base")
+    [
+      %Automedia.Movable{
+        date: ~D[2020-01-01],
+        destination: nil,
+        extension: nil,
+        source: nil,
+        time: ~T[15:01:01]
+      }
+    ]
   """
   def run(movable_files, destination_root) do
     Enum.map(movable_files, &(choose(&1, destination_root)))
   end
 
   @spec choose(Automedia.Movable.t(), binary) :: Automedia.Movable.t()
+  defp choose(%Automedia.Movable{date: nil} = movable, _destination_root), do: movable
+  defp choose(%Automedia.Movable{time: nil} = movable, _destination_root), do: movable
+  defp choose(%Automedia.Movable{extension: nil} = movable, _destination_root), do: movable
   defp choose(
     %Automedia.Movable{
       date: date,
@@ -56,7 +136,6 @@ defmodule Automedia.DestinationChooser do
     ])
     struct(movable, destination: destination)
   end
-  defp choose(%Automedia.Movable{} = movable, _destination_root), do: movable
 
   defp time_name(%Time{hour: h, minute: m, second: s, microsecond: {0, _precision}}) do
     :io_lib.format("~2..0B~2..0B~2..0B", [h, m, s])
