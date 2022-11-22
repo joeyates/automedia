00a687d588ecda3074d931666b1945c5161b7ea7
Joe Yates <joe.g.yates@gmail.com>
2022-11-22 16:04:02 +0100

    Run commands from the command line

--- lib/automedia/cli.ex
+++ lib/automedia/cli.ex
@@ -2,8 +2,37 @@ defmodule Automedia.CLI do
   use Bakeware.Script
 
   @impl Bakeware.Script
-  def main(args) do
-    IO.inspect(args)
-    0
+  def main([]) do
+    IO.puts(:stderr, "Please supply a command")
+    list_top_level_commands()
+    1
+  end
+
+  def main([command | args]) do
+    case command do
+      "android" ->
+        run(args)
+      "help" ->
+        list_top_level_commands()
+        0
+      _ ->
+        IO.puts(:stderr, "Unknown command: '#{command}'")
+        1
+    end
+  end
+
+  defp list_top_level_commands do
+    IO.puts("automedia android|fit|nextcloud|signal|whats_app ARGS")
+  end
+
+  defp run(args) do
+    try do
+      Automedia.Android.CLI.run(args)
+    rescue e in
+      ArgumentError ->
+        message = "Automedia Error: #{e.message}"
+        IO.puts(:stderr, message)
+        1
+    end
   end
 end
