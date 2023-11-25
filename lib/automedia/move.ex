2fcaf2130e0db3c2ae85a221ef429e8c6a2c94cd
Joe Yates <joe.g.yates@gmail.com>
2023-11-25 10:41:50 +0100

    Extract functions

--- lib/automedia/move.ex
+++ lib/automedia/move.ex
@@ -9,20 +9,33 @@ defmodule Automedia.Move do
 
   @callback move(Automedia.Movable.t(), keyword()) :: {:ok}
 
-  def move(%Automedia.Movable{source: source, destination: destination}, options) do
+  def move(%Automedia.Movable{destination: destination} = movable, options) do
+    exists = @file_module.regular?(destination)
+    if exists do
+      handle_existing(movable, options)
+    else
+      do_move(movable, options)
+    end
+
+    {:ok}
+  end
+
+  defp handle_existing(movable, options) do
+    destination = movable.destination
+    source = movable.source
+    Logger.info "Skipping move of '#{source}' to '#{destination}' - destination file already exists"
+  end
+
+  defp do_move(movable, options) do
+    destination = movable.destination
+    source = movable.source
     dry_run = Keyword.get(options, :dry_run, false)
     path = Path.dirname(destination)
     if !@file_module.dir?(path) do
       Logger.info "Creating directory '#{path}'"
       if !dry_run, do: @file_module.mkdir_p!(path)
     end
-    if @file_module.regular?(destination) do
-      Logger.info "Skipping move of '#{source}' to '#{destination}' - destination file already exists"
-    else
-      Logger.info "Moving '#{source}' to '#{destination}'"
-      if !dry_run, do: @file_module.rename!(source, destination)
-    end
-
-    {:ok}
+    Logger.info "Moving '#{source}' to '#{destination}'"
+    if !dry_run, do: @file_module.rename!(source, destination)
   end
 end
